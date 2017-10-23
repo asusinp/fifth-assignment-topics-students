db1cbb18c22e4e673c99e377de66025829d97e00
iaw21752927 <alexsusin1998@gmail.com>
2017-10-23 17:10:47 +0200

    added main class

---
+++ src/org/escoladeltreball/fifthassignment/Main.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package org.escoladeltreball.fifthassignment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author iaw21752927
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+//		List<String> list = Arrays.asList("alfa", "charlie", "bravo", "alfa");
+//		//list.add(null);
+//		List<String> list2 = new ArrayList<>(list);
+//		//list2.add(null);
+//		//Collections.sort(list2);
+//		System.out.println(list2);
+//		System.out.println(list);
+//		Set<String> set = new HashSet<>(list);
+//		set.add("charlie");
+//		set.add(null);
+//		set.remove("alfa");
+//		set.remove("sadsd");
+//		for (String string : set) {
+//			System.out.println(string);
+//		}
+//		System.out.println(set);
+		
+		
+//		Map<Long, String> map = new HashMap<>();
+//		map.put(2964111L, "alex");
+//		map.put(2964114L, "carlos");
+//		map.put(2964129L, "aleix");
+//		map.put(2964193L, "marc");
+//		map.put(2964126L, "marc");
+//		System.out.println(map.get(2964111L)); //una clave que no existe da como resultado null, no una excepcion
+//		map.remove(2964111L);
+//		System.out.println(map.get(2964111L));
+//		Set<Long> keys = map.keySet();
+//		System.out.println(keys);
+//		Collection<String> c = map.values();
+//		System.out.println(c);
+		
+		
+//		Map<Boolean, List<String>> map = new HashMap<>();
+//		List<String> l1 = Arrays.asList("ana", "joan", "marta");
+//		List<String> l2 = Arrays.asList("miquel", "montse", "carlos");
+//		map.put(Boolean.TRUE, l1);
+//		map.put(Boolean.FALSE, l2);
+
+	}
+
+}
